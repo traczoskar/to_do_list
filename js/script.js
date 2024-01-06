f5b7a1b3c059cbcedeb669c7b6ff9c035e7ae0b9
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-01-06 10:54:39 +0100

    adding new tasks feature, showing that task is done or not

--- js/script.js
+++ js/script.js
@@ -17,17 +17,43 @@
 
         for (const task of taskList) {
             htmlString += `
-            <li>
+            <li class= "listItem ${task.done ? "listItem--done" : ""}">
             ${task.content}
             </li>`
         }
-        
+
         document.querySelector(".js-tasks").innerHTML = htmlString;
     }
 
+    const addNewTask = (newTaskContent) => {
+
+        taskList.push({
+            content: newTaskContent
+        })
+
+        render();
+
+    }
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const newTaskContent = document.querySelector(".js-input").value.trim();
+
+        if (newTaskContent === "") {
+            return;
+        }
+
+        addNewTask(newTaskContent);
+    };
 
     const init = () => {
         render();
+
+        const form = document.querySelector(".js-form");
+
+        form.addEventListener("submit", onFormSubmit);
+
     };
 
     init();
